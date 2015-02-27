2f41a7f312af0b79c024489aa84791fd09c74f1a
Mechanolith <j.kuskoff@hotmail.com>
2015-02-27 17:29:55 +1000

    Core Res Management, Upgrades, Basic Interface
    The core systems are all in place, as well as the two basic upgrades
    (more workers, bigger tower). The game also has a basic text interface.

---
+++ Assets/A_Worker_God.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class A_Worker_God : MonoBehaviour {
+//	public A_Resource_God resGod;
+//	public int maxWorkers;
+//	public int freeWorkers;
+//
+//	void Start () {
+//		resGod = gameObject.GetComponent<A_Resource_God>();
+//	}
+//	
+//
+//	void Update () {
+//	
+//	}
+}
---
+++ Assets/I_AssignWorker_Button.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class I_AssignWorker_Button : MonoBehaviour {
+	public A_Resource_God resGod;
+	public bool addWorker;
+	public int resNum;
+	public Resource.ResType resName;
+	public TextMesh thisText;
+
+	void Start () {
+		resGod = GameObject.Find("A_Game_Logic").GetComponent<A_Resource_God>();
+
+		for(int i = 0; i < resGod.resList.Count; i++){
+			if(resGod.resList[i].name == resName){
+				resNum = i;
+			}
+		}
+
+		thisText = gameObject.GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseOver(){
+		thisText.color = Color.red;
+	}
+
+	void OnMouseExit(){
+		thisText.color = Color.white;
+	}
+
+	void OnMouseDown(){
+		if(addWorker){
+			resGod.AddWorker(resGod.resList[resNum]);
+		}
+		else{
+			resGod.RemoveWorker(resGod.resList[resNum]);
+		}
+	}
+}
