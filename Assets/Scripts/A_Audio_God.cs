28bf84f2c1e165bb4fd75b92f4d03260b5262690
Mechanolith <j.kuskoff@hotmail.com>
2015-03-02 22:26:14 +1000

    Audio Management
    Added early game BGM. Added a system to fade in each successive layer of
    the BGM. BGM fade currently works on a timer, but can be set to work on
    command from any script.

---
+++ Assets/Scripts/A_Audio_God.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class A_Audio_God : MonoBehaviour {
+	public List<AudioSource> AudioProphets = new List<AudioSource>();
+	public float lerpFactor;
+	public float lerpTimeStep;
+	public float testTimer;
+	private float lerpTimer;
+	private int state = 0;
+
+	void Start () {
+		for (int i = 0; i < 4; i++){
+			AudioProphets.Add (GameObject.Find("A_Music_Prophet_"+i).GetComponent<AudioSource>());
+		}
+
+		testTimer = 7;
+		lerpTimer = lerpTimeStep;
+	}
+
+	void Update () {
+		switch(state){
+			case 1:
+				if(lerpTimer <=0 && AudioProphets[1].volume < 0.9f){
+					AudioProphets[1].volume = Mathf.Lerp(AudioProphets[1].volume,1,lerpFactor);
+					lerpTimer = lerpTimeStep;
+//					print("Audio 1 Vol: " + AudioProphets[1].volume);
+				}
+				break;
+
+			case 2:
+				if(lerpTimer <=0 && AudioProphets[2].volume < 0.9f){
+					AudioProphets[2].volume = Mathf.Lerp(AudioProphets[2].volume,1,lerpFactor);
+					lerpTimer = lerpTimeStep;
+//					print("Audio 2 Vol: " + AudioProphets[2].volume);
+				}
+				break;
+
+			case 3:
+				if(lerpTimer <=0 && AudioProphets[3].volume < 0.9f){
+					AudioProphets[3].volume = Mathf.Lerp(AudioProphets[3].volume,1,lerpFactor);
+					lerpTimer = lerpTimeStep;
+//					print("Audio 3 Vol: " + AudioProphets[3].volume);
+				}
+				break;
+			default:
+				break;
+		}
+
+		testTimer -= Time.deltaTime;
+		if(testTimer <= 0){
+			AdvanceAudio();
+			testTimer = 7;
+		}
+
+		lerpTimer -= Time.deltaTime;
+	}
+
+	public void AdvanceAudio(){
+		state ++;
+	}
+}
---
+++ Assets/Scripts/I_Upgrade_Button.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class I_Upgrade_Button : MonoBehaviour {
+	public A_Resource_God resGod;
+	public string upgName;
+	public Upgrade upg;
+	public TextMesh thisText;
+
+	void Start () {
+		resGod = GameObject.Find("A_Game_Logic").GetComponent<A_Resource_God>();
+		thisText = gameObject.GetComponent<TextMesh>();
+	}
+
+	void Update () {
+		if (upgName == "Worker"){
+			upg = resGod.newWorker;
+		}
+		else if (upgName == "Level"){
+			upg = resGod.newLevel;
+		}
+	}
+
+	void OnMouseOver(){
+		thisText.color = Color.red;
+	}
+	
+	void OnMouseExit(){
+		thisText.color = Color.white;
+	}
+
+	void OnMouseDown(){
+		resGod.buyUpgrade(upg);
+	}
+}
