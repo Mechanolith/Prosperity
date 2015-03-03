cce4f2ac06841c8a4abbd9cfb8d2a42c9550c106
Mechanolith <j.kuskoff@hotmail.com>
2015-03-03 14:29:45 +1000

    Workers & Worldspace
    Added workers and worker AI to the game. Workers will now physically
    appear and disappear from the world. Other physical objects such as the
    ground and tower pieces were added to the worldspace.

--- Assets/Scripts/A_Resource_God.cs
+++ Assets/Scripts/A_Resource_God.cs
@@ -7,9 +7,12 @@ public class A_Resource_God : MonoBehaviour {
 	public List<Resource> resList = new List<Resource>();
 
 	public int maxWorkers, freeWorkers;
-	//public Resource wood, stone, iron, gold, bones;
 	public Upgrade newWorker, newLevel;
 	public float resTimer;
+	public GameObject towerBase, towerBody, towerCap;
+	public GameObject currentWorker, worker;
+	public List<GameObject> workForce = new List<GameObject> ();
+	private Vector3 towerPos;
 	
 	void Awake(){
 		resList.Add(new Resource(Resource.ResType.Wood,50000,10000,10,0));
@@ -20,6 +23,8 @@ public class A_Resource_God : MonoBehaviour {
 
 		newWorker = new Upgrade(Upgrade.UpgType.newWorker,0,0,0,100,0,1,1);
 		newLevel = new Upgrade(Upgrade.UpgType.newLevel,10,10,10,0,0,1,1);
+
+		towerPos = new Vector3 (0f, -0.18f, -3.25f);
 	}
 
 	void Start () {
@@ -27,19 +32,17 @@ public class A_Resource_God : MonoBehaviour {
 	}
 
 	void Update () {
-		resTimer -= Time.deltaTime;
-		if(resTimer <= 0){
-			for(int i = 0; i < resList.Count; i++){
-				if(resList[i].supply > (resList[i].workers * resList[i].rate)){
-					resList[i].current += resList[i].workers * resList[i].rate;
-					resList[i].supply -= resList[i].workers * resList[i].rate;
-				}
-				else{
-					resList[i].current += resList[i].supply;
-					resList[i].supply = 0;
-				}
-			}
-			resTimer = 15;
+
+	}
+
+	public void GetResource(int resNo){
+		if(resList[resNo].supply > (resList[resNo].rate)){
+			resList[resNo].current += resList[resNo].rate;
+			resList[resNo].supply -= resList[resNo].rate;
+		}
+		else{
+			resList[resNo].current += resList[resNo].supply;
+			resList[resNo].supply = 0;
 		}
 	}
 
@@ -74,6 +77,7 @@ public class A_Resource_God : MonoBehaviour {
 		}
 
 		if(upg.name == Upgrade.UpgType.newLevel){
+			ConstructTower();
 			for(int i = 0; i < newLevel.costList.Count; i++){
 				newLevel.costList[i] = Mathf.RoundToInt(newLevel.costList[i] * 1.15f);
 			}
@@ -85,13 +89,53 @@ public class A_Resource_God : MonoBehaviour {
 		if(freeWorkers > 0){
 			res.workers++;
 			freeWorkers--;
+			currentWorker = Instantiate(worker,new Vector3(0f,-0.8f,-5f), Quaternion.identity) as GameObject;
+			currentWorker.GetComponent<P_Worker_AI>().mustCollect = res.name;
+			workForce.Add(currentWorker);
 		}
 	}
 
 	public void RemoveWorker(Resource res){
+		P_Worker_AI workerScript;
+		bool removed = false;
+		GameObject tempWorker = null;
+		int tempNum = 0;
+
 		if(res.workers > 0){
 			res.workers--;
 			freeWorkers++;
+			for(int k = 0; k < workForce.Count; k++){
+				workerScript = workForce[k].GetComponent<P_Worker_AI>();
+				if(workerScript.mustCollect == res.name && !workerScript.carrying){
+					Destroy(workForce[k]);
+					workForce.RemoveAt(k);
+					removed = true;
+					break;
+				}
+			}
+
+			if(!removed){
+				for(int a = 0; a < workForce.Count; a++){
+					workerScript = workForce[a].GetComponent<P_Worker_AI>();
+					if(workerScript.mustCollect == res.name){
+						tempWorker = workForce[a];
+						tempNum = a;
+					}
+				}
+				Destroy(tempWorker);
+				workForce.RemoveAt(tempNum);
+			}
 		}
+	}
+
+	void ConstructTower(){
+		if(newLevel.iteration == 1){
+			Instantiate(towerBase,towerPos,Quaternion.identity);
+		}
+		else if (newLevel.iteration > 1){
+			Instantiate(towerBody,towerPos,Quaternion.identity);
+		}
+
+		towerPos += new Vector3 (0,2,0);
 	}
 }
---
+++ Assets/Scripts/P_Worker_AI.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P_Worker_AI : MonoBehaviour {
+	public float moveSpeed;
+	public Resource.ResType mustCollect;
+	public bool dying = false;
+	public bool carrying = false;
+	private int resNo;
+	private A_Resource_God resGod;
+	private int rand;
+
+
+	void Start () {
+		resGod = GameObject.Find ("A_Game_Logic").GetComponent<A_Resource_God> ();
+
+		for(int i = 0; i < resGod.resList.Count; i++){
+			if(resGod.resList[i].name == mustCollect){
+				resNo = i;
+			}
+		}
+
+		rand = Random.Range (0, 100);
+		if(rand < 50){
+			moveSpeed = 1;
+		}
+		else{
+			moveSpeed = -1;
+		}
+	}
+
+	void Update () {
+		if(!dying){
+			rigidbody.velocity = Vector3.right * moveSpeed;
+		}
+		else{
+			rigidbody.velocity = Vector3.zero;
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.tag == "End"){
+			moveSpeed *= -1;
+			carrying = true;
+		}
+		else if (col.tag == "Tower" && carrying == true){
+			resGod.GetResource(resNo);
+			moveSpeed *= -1;
+			carrying = false;
+		}
+	}
+}
